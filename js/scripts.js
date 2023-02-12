7cd40ce24de76e72c8e15234eba9d19bcc29ce1b
Tinkjimmy <tinkjimmy938@gmail.com>
2023-02-12 23:01:11 +0100

    added pokemons
    If applied this commit would have added 5 new pokemons to the pokedex app

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList =[{name:'Bulbasaur' ,height:0.7 ,types:['grass','poison']},
+                    {name:'Charmander' ,height:0.6 ,types:['fire']},
+                    {name:'Squirtle' ,height:0.5 ,types:['water']},
+                    {name:'Caterpie' ,height:0.3  ,types:['bug']},
+                    {name:'Beedrill' ,height:1 ,types:['bug','poison']}
+];
