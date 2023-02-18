dda11920381c07ba2c49f65632b4d205a97a1c7d
Tinkjimmy <tinkjimmy938@gmail.com>
2023-02-18 17:19:03 +0100

    IIFE
    i wrapped pokemonList1 in an IIFE and i created 2 methods.
    I also create a variable "list" that contains the method .getAll so that the forEach loop can access pokemonList1

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-let pokemonList1 =[{name:'Bulbasaur' ,height:0.7 ,types:['grass','poison']},
+let pokemoRepository = (function(){
+    let pokemonList1 =[{name:'Bulbasaur' ,height:0.7 ,types:['grass','poison']},
                     {name:'Charmander' ,height:0.6 ,types:['fire']},
                     {name:'Squirtle' ,height:0.5 ,types:['water']},
                     {name:'Caterpie' ,height:0.3  ,types:['bug']},
                     {name:'Beedrill' ,height:1 ,types:['bug','poison']}
-];
- // printArrayDetails function declaration
-// function printArrayDetails(list){
-// for(let i = 0; i<list.length ; i++){
-//     if( list[i].height > 0.9){
-//         // with this code, if the height is above 0.9 m it will write :"wow that's big"
-//         document.write("<p>" + list[i].name + " height : " + list[i].height + " m , wow that's big! </p>")
-//     }else{
-//         document.write ("<p>" + list[i].name + " height : " + list[i].height + " m </p>");
-//         }
-//     }
-// }
-// printArrayDetails(pokemonList1);
+                    ];
+           
+                    return {
+                        add: function(pokemon) {
+                          pokemonList1.push(pokemon);
+                        },
+                        getAll: function() {
+                          return pokemonList1;
+                        }
+                      };
 
- pokemonList1.forEach(function(list){
+})();
+
+console.log(  pokemoRepository.getAll()); 
+ pokemoRepository.add({ name: 'Lapras' });
+ //this  line is to use the getAll method in the forEach loop
+let list = pokemoRepository.getAll()
+
+
+
+ list.forEach(function(list){
         if( list.height > 0.9){
             // with this code, if the height is above 0.9 m it will write :"wow that's big"
             document.write("<p>" + list.name + " height : " + list.height + " m , wow that's big! </p>")
