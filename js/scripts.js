0214750a6d5100ddd6dc4a0ff913709c06e5522e
Tinkjimmy <tinkjimmy938@gmail.com>
2023-02-23 15:57:11 +0100

    button list
    i've implemented code that would create a list of buttons with the pokemon's specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,52 @@
-let pokemoRepository = (function(){
-    let pokemonList1 =[{name:'Bulbasaur' ,height:0.7 ,types:['grass','poison']},
-                    {name:'Charmander' ,height:0.6 ,types:['fire']},
-                    {name:'Squirtle' ,height:0.5 ,types:['water']},
-                    {name:'Caterpie' ,height:0.3  ,types:['bug']},
-                    {name:'Beedrill' ,height:1 ,types:['bug','poison']}
-                    ];
-           
-                    return {
-                        add: function(pokemon) {
-                          pokemonList1.push(pokemon);
-                        },
-                        getAll: function() {
-                          return pokemonList1;
+let pokemonRepository = (function(){
+    let repository =[{name:'Bulbasaur' ,height:0.7 ,types:['grass','poison']},
+                        {name:'Charmander' ,height:0.6 ,types:['fire']},
+                        {name:'Squirtle' ,height:0.5 ,types:['water']},
+                        {name:'Caterpie' ,height:0.3  ,types:['bug']},
+                        {name:'Beedrill' ,height:1 ,types:['bug','poison']},
+                                      ];
+
+                        function getAll() {
+                                      return repository;
+                                    }  
+                            
+                                      
+                        function addListItem(pokemon){
+                        
+                            let pokemonList = document.querySelector('.pokemon-list');
+                            let listItem = document.createElement('li');
+                            let button =document.createElement('button');
+                            button.innerText = pokemon.name;
+                            button.classList.add('pokeButton');
+                            listItem.appendChild(button);
+                            pokemonList.appendChild(listItem);
+                            button.addEventListener('click', showDetails(pokemon));}
+                                      
+                        function showDetails(pokemon){
+                          console.log(pokemon)
                         }
-                      };
+                   
+                        return {
+                        
+                        getAll: getAll,
+                        addListItem: addListItem
+                            };
+                          })();
+                        
 
-})();
-
-console.log(  pokemoRepository.getAll()); 
- pokemoRepository.add({ name: 'Lapras' });
- //this  line is to use the getAll method in the forEach loop
-let list = pokemoRepository.getAll()
+                          pokemonRepository.getAll().forEach(function(pokemon){
+                            pokemonRepository.addListItem(pokemon);
+                          });
+                          
 
 
 
- list.forEach(function(list){
-        if( list.height > 0.9){
-            // with this code, if the height is above 0.9 m it will write :"wow that's big"
-            document.write("<p>" + list.name + " height : " + list.height + " m , wow that's big! </p>")
-        }else{
-            document.write ("<p>" + list.name + " height : " + list.height + " m </p>");
-            }
-        }
- )
+
+
+
+
+        
+
    
    
 
